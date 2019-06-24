4d01026d8981cfcb6e2de3eb56284809889d20c5
olivier-dikken <olivierdikken@hotmail.fr>
2019-06-24 10:41:47 +0200

    WIP ocr match round number
    next step is match round start 00:00 timer and create array[round_index] = round_start_frame

--- video_handler.py
+++ video_handler.py
@@ -3,9 +3,17 @@ from datetime import timedelta
 
 import cv2
 import utils
+import read_metadata as meta_util
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
 
 START_TIME_DAY_1 = '2018-03-02T10:00:00.000+00:00'
 START_TIME_DAY_2 = '2018-03-04T10:00:00.000+00:00'
+
+global currentDigit
 
 
 # todo: make a dictionary that matches player names with the video file they are in P1 - P10
@@ -44,13 +52,44 @@ def read_video(fp):
 def display_video(cap, frame_number):
     # Read until video is completed
     cap.set(1, frame_number - 1)
-    while (cap.isOpened()):
+    cv2.waitKey(1000)
+    ret, frame = cap.read()
+    cv2.waitKey(1000)
+    # Display the resulting frame
+    shape = frame.shape
+    roi = [0.007, 0.05, 0.435, 0.451]  # format ymin, ymax, xmin, xmax
+    sub_frame = frame[round(roi[0] * shape[0]):round(roi[1] * shape[0]),
+                round(roi[2] * shape[1]):round(roi[3] * shape[1]), :]
+    ret, sub_frame = cv2.threshold(sub_frame, 127, 255, cv2.THRESH_BINARY)
+    scale = 3
+    # sub_frame_resized = cv2.resize(sub_frame, (round(sub_frame.shape[1] * scale), round(sub_frame.shape[0] * scale)))
+    cv2.imshow('FrameScore', sub_frame)
+
+    round_number_filename = f'digit_{currentDigit}.png'
+    cv2.imwrite(round_number_filename, sub_frame)
+    print(f'saved file {round_number_filename}')
+
+
+    while (cap.isOpened()) and 0:
         # Capture frame-by-frame
         ret, frame = cap.read()
 
         if ret == True:
             # Display the resulting frame
-            cv2.imshow('Frame', frame)
+            shape = frame.shape
+            roi = [0.007, 0.05, 0.435, 0.451] #format ymin, ymax, xmin, xmax
+            sub_frame = frame[round(roi[0]*shape[0]):round(roi[1]*shape[0]), round(roi[2]*shape[1]):round(roi[3]*shape[1]), :]
+            ret, sub_frame = cv2.threshold(sub_frame, 127, 255, cv2.THRESH_BINARY)
+            scale = 3
+            #sub_frame_resized = cv2.resize(sub_frame, (round(sub_frame.shape[1] * scale), round(sub_frame.shape[0] * scale)))
+            cv2.imshow('FrameScore', sub_frame)
+
+            round_number_filename = f'digit_{currentDigit}.png'
+            cv2.imwrite(round_number_filename, sub_frame)
+            print(f'saved file {round_number_filename}')
+
+            #cv2.imshow('Frame', frame)
+
 
             # Press Q on keyboard to  exit
             if cv2.waitKey(10) & 0xFF == ord('q'):
@@ -103,13 +142,30 @@ def get_frame_offset(json_event_datatime, json_start_datetime, fps=60):
 # return frame of even
 def sync(event, frame_dict, vid, start_date):
     start_round_frame = get_start_match(event["round"], frame_dict)
-    delta =
+
 
 
 
 
 if __name__ == '__main__':
+    ss = meta_util.StreamSync()
+    video_name = '2018-03-02_P1.mp4'
+    video_path = "C:\save\\" + video_name
     vid = read_video(video_path)
-    print(fps)
-    display_video(vid, 445500)
-    print(metadata)
+
+    m1_p1_start_frame = int(ss.data['match_1']['player_P1'])
+    print(m1_p1_start_frame)
+    round_frame = []
+    round_frame.append(m1_p1_start_frame + 5000)
+    round_frame.append(m1_p1_start_frame + 18000)
+    round_frame.append(m1_p1_start_frame + 20000)
+    round_frame.append(m1_p1_start_frame + 25000)
+    #round_test_frame = m1_p1_start_frame + 25000
+    for i in range(0, len(round_frame)):
+        currentDigit = i
+        print(f'processing round number {i}')
+        print(round_frame[i])
+        display_video(vid, round_frame[i])
+        vid = read_video(video_path)
+
+
